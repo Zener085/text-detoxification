9b8674f4ef12abf1cf87306fedb25c81db29e232
Zener_085 <57841939+Zener085@users.noreply.github.com>
2023-10-20 20:03:37 +0300

    Start
    Created all directories and files
    Started working with dataset
    No data is loaded, I will add it later

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+"""
+Source package for the project.
+
+There are three packages inside:
+    data: Scripts to download or generate data.
+    models: Scripts to train models and then use trained models to make predictions.
+    visualizations: Scripts to create exploratory and results oriented visualizations.
+"""
+__author__ = "Zener085"
+__version__ = "1.0.0"
+__license__ = "MIT"
---
+++ src/models/__init__.py
@@ -0,0 +1,18 @@
+"""
+Scripts to train models and then use trained models to make predictions.
+"""
+__author__ = "Zener085"
+__version__ = "1.0.0"
+__license__ = "MIT"
+
+
+def train():
+    """
+    Trains the model.
+    """
+
+
+def predict():
+    """
+    Predicts the output using the trained model.
+    """
